ab42d1ab91b465daae079c4e0254af7d22805b06
I-m-gopal <gopalmandol439@gmail.com>
2024-07-16 23:12:34 +0530

    Add files via upload

---
+++ Day01.js
@@ -0,0 +1,38 @@
+//Day 1: Variables and Data Types
+
+//Declare using var
+var no = 1;
+console.log(no);
+
+//Declare using let
+let str = 'Javascript';
+console.log(str);
+
+//Declare using const
+const isbool = true;
+console.log(isbool);
+
+//Data types
+let num = 1;
+let str1 = "Hi this is day1";
+let istrue = true;
+const challange = {Day:'Day01',skill:'Js'};
+let skill = ["Python","Js"];
+console.log(typeof(num));
+console.log(typeof(str1));
+console.log(typeof(istrue));
+console.log(typeof(challange));
+console.log(typeof(skill));
+
+//Reassigning
+let first = 1;
+console.log(first);
+first = 2;
+console.log(first);
+
+//Reassigning with const
+const value = 'Hi';
+console.log(value);
+//Error value
+//value = 'Hello';
+//console.log(value);
